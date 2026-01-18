23fb6397ae36198fe66ccae71026f62e5ffd595c
MontossoriVisualization <MontossoriVisualization@users.noreply.github.com>
2026-01-18 12:56:33 +0545

    object.js

--- basics0/9-object.js
+++ basics0/9-object.js
@@ -12,7 +12,7 @@ console.log(typeof LetsLearnUser);
 console.log(LetsLearnUser.email);
 console.log(LetsLearnUser["email"]);
 console.log(LetsLearnUser.lastLoginDays[1]);
-LetsLearnUser.email = "pawan_new@gmail.com";
+LetsLearnUser["email"] = "pawan_new@gmail.com";
 console.log(LetsLearnUser.email);
 console.log(LetsLearnUser.isLoggedIn);
 if(LetsLearnUser.isLoggedIn){
@@ -27,11 +27,45 @@ if(LetsLearnUser.isLoggedIn){
     LetsLearnUser.greating();
 
 
-    const anotherUser = {};
+    const anotherUser = new Object();
+
     anotherUser.name = "saugat";
     anotherUser.age = 25;
     anotherUser.email = "saugat@gmail.com";
     anotherUser.isLoggedIn = false;
     anotherUser.lastLoginDays = ["Tuesday","Friday"]; 
-    console.log(anotherUser);
     
+    console.log(anotherUser);
+    const totalUser = {...LetsLearnUser,...anotherUser};
+    console.log(totalUser);
+    console.log(Object.keys(totalUser));
+    console.log(Object.values(totalUser));
+
+    // 1 object school next class details
+    //  object student details
+
+
+    const school = {
+    class1: {
+        teacher: "Mr. Smith",
+        subject: "Mathematics",
+        students: {
+            Alice: {  age: 14 },
+            Bob: {  age: 15    },  
+            
+        },
+    }
+    ,
+    class2: {
+        teacher: "Ms. Johnson", 
+        subject: "English",
+    },
+    class3: {
+        teacher: "Mrs. Brown",
+        subject: "Science",
+    },
+    };  
+    console.log(Object.keys(school.class1.students));
+
+    //hw create an object for a student with properties like name, age, grade, subjects (array of subjects), and a method to display student details.
+
