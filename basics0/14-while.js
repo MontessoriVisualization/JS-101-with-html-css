10a878c61676521361fa2516ec943cb5ed11d158
Saugat Bagale <MontessoriVisualization@users.noreply.github.com>
2026-02-09 13:32:58 +0545

    event

---
+++ basics0/14-while.js
@@ -0,0 +1,28 @@
+//while
+// while (condition) {
+    
+// }
+let i = 0;
+while (i < 5) {
+    console.log("Count is: " + i);
+    i++;
+}
+const arr = [5, 10, 15, 20, 25];
+let sum = 0;
+let j = 0;
+while (j < arr.length) {
+    sum = sum + arr[j];
+    j++;
+}
+console.log(sum)
+
+//do while
+// do {
+    
+// } while (condition);
+
+let k = 4;
+do {
+    console.log("Value of k is: " + k);
+    k++;
+} while (k < 3);
---
+++ basics0/15-objloop.js
@@ -0,0 +1,10 @@
+const DcHero = ["Dr. Manhattan", "Captain America", "Batman", "Wonder Woman", "Spider-Man"];
+for (const a of DcHero) {
+    console.log(a);
+}
+const name= "saugat";
+for (const char of name) {
+    console.log(char);
+}
+
+// map
